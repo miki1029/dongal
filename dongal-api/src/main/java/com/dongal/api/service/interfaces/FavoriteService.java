6a7785f095712ce0cb7d24e36c3d7bcfd61e9dbc
Felika <kang8530@gmail.com>
2015-11-27 18:19:16 +0900

    edit gitignore for ignore other subprojects

--- dongal-api/src/main/java/com/dongal/api/service/interfaces/FavoriteService.java
+++
@@ -1,9 +0,0 @@
-package com.dongal.api.service.interfaces;
-
-/**
- * @author Freddi
- */
-public interface FavoriteService {
-    boolean addFavoriteToUser(int userIdx, int subscriptionIdx);
-    boolean delFavoriteFromUser(int userIdx, int subscriptionIdx);
-}
