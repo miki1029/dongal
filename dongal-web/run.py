1c70d0a184129acc736c9f1139f2160b5cbf4dd9
Felika <kang8530@gmail.com>
2015-11-29 21:58:22 +0900

    convert flask port 5000 to 9090

--- dongal-web/run.py
+++ dongal-web/run.py
@@ -90,4 +90,4 @@ def settings():
     return render_template("settings.html", title="Settings", official=data['official'], dyeon=data['dyeon'])
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=9090, debug=True)
