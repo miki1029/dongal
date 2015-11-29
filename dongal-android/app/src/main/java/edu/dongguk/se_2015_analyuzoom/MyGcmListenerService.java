d3d0e43a06def0250e575ff2391f4abe2550e30c
Felika <kang8530@gmail.com>
2015-11-29 17:37:33 +0900

    finish android push service, when app is running, background and not running

--- dongal-android/app/src/main/java/edu/dongguk/se_2015_analyuzoom/MyGcmListenerService.java
+++ dongal-android/app/src/main/java/edu/dongguk/se_2015_analyuzoom/MyGcmListenerService.java
@@ -1,13 +1,9 @@
 package edu.dongguk.se_2015_analyuzoom;
 
-import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.admin.DevicePolicyManager;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -51,7 +47,6 @@ public class MyGcmListenerService extends GcmListenerService {
     private void sendNotification(String title, String message) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
@@ -61,6 +56,7 @@ public class MyGcmListenerService extends GcmListenerService {
                 .setAutoCancel(true)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setVisibility(Notification.VISIBILITY_PUBLIC)
                 .setPriority(Notification.PRIORITY_HIGH)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
@@ -69,5 +65,16 @@ public class MyGcmListenerService extends GcmListenerService {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, notificationBuilder.build());
+
+
+        PowerManager pm = (PowerManager) getApplicationContext()
+                .getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = pm
+                .newWakeLock(
+                        (PowerManager.SCREEN_BRIGHT_WAKE_LOCK
+                                | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP),
+                        "TAG");
+        wakeLock.acquire(15000);
+
     }
 }
