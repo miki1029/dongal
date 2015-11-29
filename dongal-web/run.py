08c0af1b67137398521827f1714d8a4cab360006
miki1029 <miki1029g@gmail.com>
2015-11-29 16:28:43 +0900

    로그인 및 로그아웃 기능 추가

--- dongal-web/run.py
+++ dongal-web/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 
 import simplejson as json
 import requests
@@ -9,15 +9,39 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 # BASE_URL = "http://dna.dongguk.ac.kr/~felika/dongal/dongal-backend/"
-BASE_URL = "http://localhost:8080/view/"
+ROOT_BASE_URL = "http://localhost:8080/"
+SESSION_BASE_URL = ROOT_BASE_URL + "session/"
+VIEW_BASE_URL = ROOT_BASE_URL + "view/"
 
-@app.before_request
-def load_user():
-    session["userIdx"] = "2"
+# @app.before_request
+# def load_user():
+#     session["userIdx"] = "2"
 
+@app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index2.html", title="Index")
+    if hasattr(session, 'userIdx'):
+        return redirect("home")
+    else:
+        return render_template("index2.html", title="Index")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title="Login")
+
+@app.route("/loginProcess", methods=['POST'])
+def login_process():
+    email = request.form['email']
+    password = request.form['password']
+    url = SESSION_BASE_URL + "login?email=" + email + "&password=" + password
+    data = json.loads(requests.get(url=SESSION_BASE_URL + "login?email=" + email + "&password=" + password).text)
+    session["userIdx"] = str(data["idx"])
+    return redirect(url_for('home'))
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 @app.route("/joinSelect")
 def join_select():
@@ -31,20 +55,19 @@ def join_by_mail():
 def join():
     return render_template("join.html", title="Join")
 
-@app.route("/")
 @app.route("/home")
 def home():
-    data = json.loads(requests.get(url=BASE_URL+"home?userIdx="+session["userIdx"]).text)
+    data = json.loads(requests.get(url=VIEW_BASE_URL + "home?userIdx=" + session["userIdx"]).text)
     return render_template("home.html", title="Home", userInfo=data['userInfo'], alarms=data['posts'])
 
 @app.route("/list")
 def list():
-    data = json.loads(requests.get(url=BASE_URL+"list?userIdx="+session["userIdx"]).text)
+    data = json.loads(requests.get(url=VIEW_BASE_URL + "list?userIdx=" + session["userIdx"]).text)
     return render_template("list.html", title="List", favorites=data['posts'])
 
 @app.route("/favorite")
 def favorite():
-    data = json.loads(requests.get(url=BASE_URL+"favorite?userIdx="+session["userIdx"]).text)
+    data = json.loads(requests.get(url=VIEW_BASE_URL + "favorite?userIdx=" + session["userIdx"]).text)
     return render_template("favorite.html", title="Favorite", favorites=data['posts'])
 
 @app.route("/more")
@@ -53,7 +76,7 @@ def more():
 
 @app.route("/settings")
 def settings():
-    data = json.loads(requests.get(url=BASE_URL+"settings?userIdx="+session["userIdx"]).text)
+    data = json.loads(requests.get(url=VIEW_BASE_URL + "settings?userIdx=" + session["userIdx"]).text)
     return render_template("settings.html", title="Settings", official=data['official'], dyeon=data['dyeon'])
 
 if __name__ == "__main__":
